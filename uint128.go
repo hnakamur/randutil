7a03b845d1cadea7b66ba17ad14e2a549670d890
Hiroaki Nakamura <hnakamur@gmail.com>
2017-06-05 00:26:41 +0900

    Implement AppendUint128InZeroPaddedBase36 without using math/big.Int
    $ go test -count=10 -run=NONE -bench BenchmarkUint128ToZeroPaddedBase36 -benchmem -cpuprofile=cpu-new.prof | tee new.log
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    121299 ns/op	     320 B/op	      10 allocs/op
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    121328 ns/op	     320 B/op	      10 allocs/op
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    120664 ns/op	     320 B/op	      10 allocs/op
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    121178 ns/op	     320 B/op	      10 allocs/op
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    121160 ns/op	     320 B/op	      10 allocs/op
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    121333 ns/op	     320 B/op	      10 allocs/op
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    121328 ns/op	     320 B/op	      10 allocs/op
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    120807 ns/op	     320 B/op	      10 allocs/op
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    120994 ns/op	     320 B/op	      10 allocs/op
    BenchmarkUint128ToZeroPaddedBase36-2   	   10000	    120950 ns/op	     320 B/op	      10 allocs/op
    PASS
    ok  	github.com/hnakamur/randutil	12.296s
    
    $ benchstat old.log new.log
    name                         old time/op    new time/op    delta
    Uint128ToZeroPaddedBase36-2    7.67µs ± 5%  121.10µs ± 0%  +1479.55%  (p=0.000 n=10+10)
    
    name                         old alloc/op   new alloc/op   delta
    Uint128ToZeroPaddedBase36-2    1.60kB ± 0%    0.32kB ± 0%    -80.00%  (p=0.000 n=10+10)
    
    name                         old allocs/op  new allocs/op  delta
    Uint128ToZeroPaddedBase36-2      40.0 ± 0%      10.0 ± 0%    -75.00%  (p=0.000 n=10+10)

--- uint128.go
+++ uint128.go
@@ -1,19 +1,116 @@
 package randutil
 
-import "math/big"
-
-const digits = "0123456789abcdefghijklmnopqrstuvwxyz"
+import "encoding/binary"
 
 func AppendUint128InZeroPaddedBase36(buf []byte, v [16]byte) []byte {
 	var tmp [randomIDEncodedByteLen]byte
-	p := new(big.Int).SetBytes(v[:]).Append(tmp[:0], base)
-	for i := randomIDEncodedByteLen - len(p) - 1; i >= 0; i-- {
-		buf = append(buf, '0')
+	i := len(tmp) - 1
+	n := bigEndianUint128(v[:])
+	for !n.IsZero() {
+		var r uint8
+		n, r = divmodUint128ByUint8(n, base)
+		tmp[i] = digits[r]
+		i--
 	}
-	return append(buf, p...)
+	for ; i >= 0; i-- {
+		tmp[i] = '0'
+	}
+	return append(buf, tmp[:]...)
 }
 
 func Uint128ToZeroPaddedBase36(v [16]byte) string {
 	var buf [randomIDEncodedByteLen]byte
 	return string(AppendUint128InZeroPaddedBase36(buf[:0], v))
+}
+
+type uint128 struct {
+	u [2]uint64
+}
+
+func bigEndianUint128(v []byte) uint128 {
+	return uint128{
+		[2]uint64{
+			binary.BigEndian.Uint64(v[:8]),
+			binary.BigEndian.Uint64(v[8:16]),
+		},
+	}
+}
+
+const digits = "0123456789abcdefghijklmnopqrstuvwxyz"
+
+func (u *uint128) AppendHex(buf []byte) []byte {
+	var tmp [32]byte
+	i := len(tmp)
+	if u.u[0] == 0 {
+		n := u.u[1]
+		if n == 0 {
+			return append(buf, '0')
+		}
+
+		for n > 0 {
+			i--
+			tmp[i] = digits[n&0x0f]
+			n >>= 4
+		}
+		return append(buf, tmp[i:]...)
+	} else {
+		n := u.u[1]
+		for j := 15; j >= 0; j-- {
+			i--
+			tmp[i] = digits[n&0x0f]
+			n >>= 4
+		}
+
+		n = u.u[0]
+		for n > 0 {
+			i--
+			tmp[i] = digits[n&0x0f]
+			n >>= 4
+		}
+		return append(buf, tmp[i:]...)
+	}
+}
+
+func (u *uint128) Hex() string {
+	var buf [32]byte
+	return string(u.AppendHex(buf[:]))
+}
+
+// https://en.wikipedia.org/wiki/Division_algorithm#Integer_division_.28unsigned.29_with_remainder
+func divmodUint128ByUint8(n uint128, d uint8) (q uint128, r uint8) {
+	if d == 0 {
+		panic("division by zero")
+	}
+
+	if n.u[0] != 0 {
+		m := uint64(0x8000000000000000)
+		for i := 63; i >= 0; i-- {
+			r <<= 1
+			if n.u[0]&m != 0 {
+				r |= 1
+			}
+			if r >= d {
+				r -= d
+				q.u[0] |= m
+			}
+			m >>= 1
+		}
+	}
+	m := uint64(0x8000000000000000)
+	for i := 63; i >= 0; i-- {
+		r <<= 1
+		if n.u[1]&m != 0 {
+			r |= 1
+		}
+		if r >= d {
+			r -= d
+			q.u[1] |= m
+		}
+		m >>= 1
+	}
+	return
+}
+
+func (n *uint128) IsZero() bool {
+	return n.u[0] == 0 && n.u[1] == 0
 }
--- uint128_test.go
+++ uint128_test.go
@@ -1,10 +1,75 @@
-package randutil_test
+package randutil
 
 import (
 	"testing"
-
-	"github.com/hnakamur/randutil"
 )
+
+func TestUint128AppendHex(t *testing.T) {
+	testCases := []struct {
+		v    [16]byte
+		want string
+	}{
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			"0",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			"1",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x0f},
+			"f",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x10},
+			"10",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x12},
+			"12",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff},
+			"ff",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+			"100",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+			"101",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0x10},
+			"110",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+			"ffffffffffffffff",
+		},
+		{
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+			"10000000000000000",
+		},
+		{
+			[16]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe},
+			"fffffffffffffffffffffffffffffffe",
+		},
+		{
+			[16]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+			"ffffffffffffffffffffffffffffffff",
+		},
+	}
+	for _, tc := range testCases {
+		n := bigEndianUint128(tc.v[:])
+		got := string(n.AppendHex(nil))
+		if got != tc.want {
+			t.Errorf("got %q, want %q", got, tc.want)
+		}
+	}
+}
 
 func TestUint128ToZeroPaddedBase36(t *testing.T) {
 	testCases := []struct {
@@ -36,7 +101,7 @@ func TestUint128ToZeroPaddedBase36(t *testing.T) {
 			"0000000000000000000000100",
 		},
 		{
-			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255},
+			[16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
 			"0000000000003w5e11264sgsf",
 		},
 		{
@@ -44,16 +109,16 @@ func TestUint128ToZeroPaddedBase36(t *testing.T) {
 			"0000000000003w5e11264sgsg",
 		},
 		{
-			[16]byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254},
+			[16]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe},
 			"f5lxx1zz5pnorynqglhzmsp32",
 		},
 		{
-			[16]byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+			[16]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
 			"f5lxx1zz5pnorynqglhzmsp33",
 		},
 	}
 	for _, tc := range testCases {
-		got := randutil.Uint128ToZeroPaddedBase36(tc.v)
+		got := Uint128ToZeroPaddedBase36(tc.v)
 		if got != tc.want {
 			t.Errorf("got %s, want %s", got, tc.want)
 		}
