8d2ba8c894ecce77ac5260fa649e9274673a94a4
Hiroaki Nakamura <hnakamur@gmail.com>
2017-08-11 16:29:56 +0900

    Add Uint64

--- intner.go
+++ intner.go
@@ -3,6 +3,9 @@ package randutil
 // Intner is the interface that returns a non-negative pseudo random int in the specified range.
 //
 // Intn returns, as an int, a non-negative pseudo-random number in [0,n). It panics if n <= 0.
+//
+// Uint64 returns a pseudo-random 64-bit value as a uint64.
 type Intner interface {
 	Intn(n int) (int, error)
+	Uint64() (uint64, error)
 }
