4805d49a180951e53ab77429b54b3da0eab5dc7e
Hiroaki Nakamura <hnakamur@gmail.com>
2017-08-12 11:13:57 +0900

    Modify General to check g only once

--- general.go
+++ general.go
@@ -5,32 +5,49 @@ import (
 	"fmt"
 )
 
-// General returns a general discrete distributed random value.
-func General(intner Intner, g []float64) (float64, error) {
+// General is a general discrete distributed random value generator.
+type General struct {
+	intner Intner
+	g      []float64
+}
+
+// NewGeneral creates a general discrete distributed random value generator.
+//
+// The rules for g are:
+// * The lenght of g must be greater than or equal to 2.
+// * The first element of g must be 0.
+// * The last element of g must be 1.
+// * The g must be monotonously increasing, that is, g[i] >= g[i-1] for any i.
+func NewGeneral(intner Intner, g []float64) (*General, error) {
 	if len(g) < 2 {
-		return 0, errors.New("length of g must be greater than or equal to 2")
+		return nil, errors.New("length of g must be greater than or equal to 2")
 	}
 	if g[0] != 0 {
-		return 0, errors.New("the first element of g must be 0")
+		return nil, errors.New("the first element of g must be 0")
 	}
 	if g[len(g)-1] != 1 {
-		return 0, errors.New("the last element of g must be 1")
+		return nil, errors.New("the last element of g must be 1")
 	}
 	for i := 1; i < len(g)-1; i++ {
 		if g[i] < g[i-1] {
-			return 0, fmt.Errorf("g must be monotonously increasing, g[%d]=%f < g[%d]=%f", i, g[i], i-1, g[i-1])
+			return nil, fmt.Errorf("g must be monotonously increasing, g[%d]=%f < g[%d]=%f", i, g[i], i-1, g[i-1])
 		}
 	}
+	return &General{intner: intner, g: g}, nil
+}
 
+// Next returns a general discrete distributed random value.
+func (g *General) Next() (float64, error) {
 	// http://www.ishikawa-lab.com/montecarlo/4shou.html
-	x0 := 1 / float64(len(g))
-	r, err := Float64(intner)
+	gs := g.g
+	x0 := 1 / float64(len(gs))
+	r, err := Float64(g.intner)
 	if err != nil {
 		return 0, err
 	}
 	i := 0
-	for i < len(g) && g[i] < r {
+	for i < len(gs) && gs[i] < r {
 		i++
 	}
-	return (r-g[i-1])/(g[i]-g[i-1])*x0 + float64(i-1)*x0, nil
+	return (r-gs[i-1])/(gs[i]-gs[i-1])*x0 + float64(i-1)*x0, nil
 }
