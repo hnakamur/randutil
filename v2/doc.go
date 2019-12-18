d90037fd7c93fc6ce365b35116dffab841d71fff
Hiroaki Nakamura <hnakamur@gmail.com>
2019-12-18 22:38:20 +0900

    Delete all go files

--- v2/doc.go
+++
@@ -1,9 +0,0 @@
-// Package randutil/v2 provides a utility function to produce
-// random integer, float, bytes, and texts.
-// This package is meant for generating random inputs to be
-// used for tests or simulations.
-// It does NOT provide crypto level randomness.
-//
-// The randutil/v2 drops Intner using crypto/rand and PCG.
-// It also drops Chooser.
-package v2
--- v2/float.go
+++
@@ -1,8 +0,0 @@
-package v2
-
-// Float64 returns, as a float64, a pseudo-random number in [0.0,1.0).
-func Float64(intner Intner) float64 {
-	// https://github.com/golang/go/blob/c007ce824d9a4fccb148f9204e04c23ed2984b71/src/math/rand/rand.go#L105-L106
-	u := intner.Int63n(1 << 53)
-	return float64(u) / (1 << 53)
-}
