a38f93a675e91a00db157b71e2f7e9f727c46825
Hiroaki Nakamura <hnakamur@gmail.com>
2020-01-15 16:38:40 +0900

    Add benchmark of Int63n and Int31n in math/rand and exp/rand
    $ go test -run=none -bench=. -benchmem -cpuprofile=cpu.prof
    goos: linux
    goarch: amd64
    pkg: github.com/hnakamur/randutil/v3
    BenchmarkInt63n/mathMaxInt64-4          47586747                25.2 ns/op             0 B/op          0 allocs/op
    BenchmarkInt63n/expMaxInt64-4           42108542                26.0 ns/op             0 B/op          0 allocs/op
    BenchmarkInt63n/math2147483647-4        44417109                25.5 ns/op             0 B/op          0 allocs/op
    BenchmarkInt63n/exp2147483647-4         67835594                17.5 ns/op             0 B/op          0 allocs/op
    BenchmarkInt31n/mathMaxInt32-4          89317851                13.1 ns/op             0 B/op          0 allocs/op
    BenchmarkInt31n/expMaxInt32-4           64581254                17.7 ns/op             0 B/op          0 allocs/op
    BenchmarkInt31n/math2147483646-4        88637041                13.0 ns/op             0 B/op          0 allocs/op
    BenchmarkInt31n/exp2147483646-4         68898139                17.5 ns/op             0 B/op          0 allocs/op
    PASS
    ok      github.com/hnakamur/randutil/v3 11.418s

---
+++ benchmark_test.go
@@ -0,0 +1,76 @@
+package randutil
+
+import (
+	"math"
+	"math/rand"
+	"testing"
+	"time"
+
+	erand "golang.org/x/exp/rand"
+)
+
+func BenchmarkInt63n(b *testing.B) {
+	seed := time.Now().UnixNano()
+
+	b.Run("mathMaxInt64", func(b *testing.B) {
+		const n = math.MaxInt64
+		rnd := rand.New(rand.NewSource(seed))
+		for i := 0; i < b.N; i++ {
+			rnd.Int63n(n)
+		}
+	})
+	b.Run("expMaxInt64", func(b *testing.B) {
+		const n = math.MaxInt64
+		rnd := erand.New(erand.NewSource(uint64(seed)))
+		for i := 0; i < b.N; i++ {
+			rnd.Int63n(n)
+		}
+	})
+	b.Run("math2147483647", func(b *testing.B) {
+		const n = 1<<31 - 1
+		rnd := rand.New(rand.NewSource(seed))
+		for i := 0; i < b.N; i++ {
+			rnd.Int63n(n)
+		}
+	})
+	b.Run("exp2147483647", func(b *testing.B) {
+		const n = 1<<31 - 1
+		rnd := erand.New(erand.NewSource(uint64(seed)))
+		for i := 0; i < b.N; i++ {
+			rnd.Int63n(n)
+		}
+	})
+}
+
+func BenchmarkInt31n(b *testing.B) {
+	seed := time.Now().UnixNano()
+
+	b.Run("mathMaxInt32", func(b *testing.B) {
+		const n = math.MaxInt32
+		rnd := rand.New(rand.NewSource(seed))
+		for i := 0; i < b.N; i++ {
+			rnd.Int31n(n)
+		}
+	})
+	b.Run("expMaxInt32", func(b *testing.B) {
+		const n = math.MaxInt32
+		rnd := erand.New(erand.NewSource(uint64(seed)))
+		for i := 0; i < b.N; i++ {
+			rnd.Int31n(n)
+		}
+	})
+	b.Run("math2147483646", func(b *testing.B) {
+		const n = 1<<31 - 1 - 1
+		rnd := rand.New(rand.NewSource(seed))
+		for i := 0; i < b.N; i++ {
+			rnd.Int31n(n)
+		}
+	})
+	b.Run("exp2147483646", func(b *testing.B) {
+		const n = 1<<31 - 1 - 1
+		rnd := erand.New(erand.NewSource(uint64(seed)))
+		for i := 0; i < b.N; i++ {
+			rnd.Int31n(n)
+		}
+	})
+}
