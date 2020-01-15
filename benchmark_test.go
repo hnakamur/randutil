9ae05fb6d11cc673bb8d25739ca99f11674ae05e
Hiroaki Nakamura <hnakamur@gmail.com>
2020-01-15 16:44:04 +0900

    Add benchmark of Intn
    $ go test -run=none -bench=. -benchmem -cpuprofile=cpu.prof
    goos: linux
    goarch: amd64
    pkg: github.com/hnakamur/randutil/v3
    BenchmarkInt63n/mathMaxInt64-4          47693278                25.1 ns/op             0 B/op          0 allocs/op
    BenchmarkInt63n/expMaxInt64-4           45983836                26.1 ns/op             0 B/op          0 allocs/op
    BenchmarkInt63n/math2147483647-4        47281989                25.3 ns/op             0 B/op          0 allocs/op
    BenchmarkInt63n/exp2147483647-4         62685103                17.7 ns/op             0 B/op          0 allocs/op
    BenchmarkInt31n/mathMaxInt32-4          87346663                13.4 ns/op             0 B/op          0 allocs/op
    BenchmarkInt31n/expMaxInt32-4           67753634                17.8 ns/op             0 B/op          0 allocs/op
    BenchmarkInt31n/math2147483646-4        89554389                12.8 ns/op             0 B/op          0 allocs/op
    BenchmarkInt31n/exp2147483646-4         66659641                17.4 ns/op             0 B/op          0 allocs/op
    BenchmarkIntn/mathMaxInt64-4            41297737                28.7 ns/op             0 B/op          0 allocs/op
    BenchmarkIntn/expMaxInt64-4             43655096                25.7 ns/op             0 B/op          0 allocs/op
    BenchmarkIntn/math2147483647-4          79729692                15.2 ns/op             0 B/op          0 allocs/op
    BenchmarkIntn/exp2147483647-4           68824585                17.4 ns/op             0 B/op          0 allocs/op
    BenchmarkIntn/mathMaxInt32-4            79484058                15.1 ns/op             0 B/op          0 allocs/op
    BenchmarkIntn/expMaxInt32-4             67829474                17.5 ns/op             0 B/op          0 allocs/op
    BenchmarkIntn/math2147483646-4          79925668                15.2 ns/op             0 B/op          0 allocs/op
    BenchmarkIntn/exp2147483646-4           64672346                17.6 ns/op             0 B/op          0 allocs/op
    PASS
    ok      github.com/hnakamur/randutil/v3 25.637s

--- benchmark_test.go
+++ benchmark_test.go
@@ -74,3 +74,64 @@ func BenchmarkInt31n(b *testing.B) {
 		}
 	})
 }
+
+func BenchmarkIntn(b *testing.B) {
+	seed := time.Now().UnixNano()
+
+	b.Run("mathMaxInt64", func(b *testing.B) {
+		const n = math.MaxInt64
+		rnd := rand.New(rand.NewSource(seed))
+		for i := 0; i < b.N; i++ {
+			rnd.Intn(n)
+		}
+	})
+	b.Run("expMaxInt64", func(b *testing.B) {
+		const n = math.MaxInt64
+		rnd := erand.New(erand.NewSource(uint64(seed)))
+		for i := 0; i < b.N; i++ {
+			rnd.Intn(n)
+		}
+	})
+	b.Run("math2147483647", func(b *testing.B) {
+		const n = 1<<31 - 1
+		rnd := rand.New(rand.NewSource(seed))
+		for i := 0; i < b.N; i++ {
+			rnd.Intn(n)
+		}
+	})
+	b.Run("exp2147483647", func(b *testing.B) {
+		const n = 1<<31 - 1
+		rnd := erand.New(erand.NewSource(uint64(seed)))
+		for i := 0; i < b.N; i++ {
+			rnd.Intn(n)
+		}
+	})
+	b.Run("mathMaxInt32", func(b *testing.B) {
+		const n = math.MaxInt32
+		rnd := rand.New(rand.NewSource(seed))
+		for i := 0; i < b.N; i++ {
+			rnd.Intn(n)
+		}
+	})
+	b.Run("expMaxInt32", func(b *testing.B) {
+		const n = math.MaxInt32
+		rnd := erand.New(erand.NewSource(uint64(seed)))
+		for i := 0; i < b.N; i++ {
+			rnd.Intn(n)
+		}
+	})
+	b.Run("math2147483646", func(b *testing.B) {
+		const n = 1<<31 - 1 - 1
+		rnd := rand.New(rand.NewSource(seed))
+		for i := 0; i < b.N; i++ {
+			rnd.Intn(n)
+		}
+	})
+	b.Run("exp2147483646", func(b *testing.B) {
+		const n = 1<<31 - 1 - 1
+		rnd := erand.New(erand.NewSource(uint64(seed)))
+		for i := 0; i < b.N; i++ {
+			rnd.Intn(n)
+		}
+	})
+}
